578f98a9ee3fff763946705c8b8e2c69c1b08927
JuanSeGR <jsgonzalez@belvi.co>
2025-10-17 14:16:30 -0500

    Base project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+// resources/js/app.js
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
